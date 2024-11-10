ac73cb3b3f9e6a02e399e187f82baf74dc648390
Clauda <Clauda@DESKTOP-IKD135F>
2024-11-10 12:31:34 +0200

    Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Welcome</title>
+    <style>
+        body {
+            background-color: #f8f9fa;
+            padding-top: 50px;
+        }
+        .container {
+            text-align: center;
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        .btn-custom {
+            padding: 10px 20px;
+            font-size: 16px;
+        }
+    </style>
 </head>
 <body> 
-    <p>This is the home page.</p>
-    <a href="{{ route('students.index') }}"> View Students </a> 
-    <br>
-    <a class ="btn btn-secondary" style="padding: 10px"  href="{{ route('teachers.index') }}"> Go To Teachers Page </a> 
+    <div class="container">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page where you can navigate to different sections.</p>
+        <div>
+            <a href="{{ route('students.index') }}" class="btn btn-primary btn-custom m-2">View Students</a>
+        </div>
+        <div>
+            <a href="{{ route('teachers.index') }}" class="btn btn-secondary btn-custom m-2">Go to Teachers Page</a>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
