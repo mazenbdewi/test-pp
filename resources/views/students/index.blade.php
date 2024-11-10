a9e70f3b58b23d5fc6f747a936401c29882b161c
Clauda <Clauda@DESKTOP-IKD135F>
2024-11-10 11:35:58 +0200

    Style in One File

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -36,7 +36,7 @@
                         <td>
                             <a href="{{ route('students.show', $student) }}">View</a>
                             <a href="{{ route('students.edit', $student) }}">Edit</a>
-                            <form action="{{ route('students.destroy', $student) }}" method="POST"  >
+                            <form action="{{ route('students.destroy', $student) }}" method="POST" style="display:inline;">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="button delete-button">Delete</button>
