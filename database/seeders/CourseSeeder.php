ed4c78ff9d7abb433bf82f622243f6fddef17c28
Clauda <klodaalrakkad@gmail.com>
2024-11-20 10:49:39 +0300

    COURSES SEEDER

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -12,7 +12,8 @@ class CourseSeeder extends Seeder
     
     public function run(): void
     {
-        $courses = ['Mathematics', 'Physics', 'Chemistry', 'Biology', 'Computer Science'];
+        $courses = ['Mathematics', 'Physics', 'Chemistry', 'Biology', 'Computer Science','P1 C++'
+        ,'P2 JAVA' , 'DB1','DB2'];
 
         foreach ($courses as $course) {
             Course::create(['name' => $course]);
