c881b17174ffcf8031ff6a4c24bf95e0b2ed9b20
gadeermahmoodhamdan <gadeermahmoodhamdan@gmail.com>
2024-11-23 22:40:31 -0800

    adding links to pages

--- resources/views/schools/layout.blade.php
+++ resources/views/schools/layout.blade.php
@@ -6,18 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
     <title>Schools!</title>
   </head>
   <body>
-    <h1><a  href="{{ url('/school/create') }}">Create a new school</a></h1>
+    &nbsp; &nbsp;
+    <a href="{{ url('/') }}">Home</a> &nbsp;
+    <a href="{{ url('/schools') }}">Schools</a> &nbsp;
+    <a href="{{ url('/school/create') }}">Create a new school</a>&nbsp;
+    <a href="{{ url('/schools/divisions') }}">Divisions</a>&nbsp;
+    <a href="{{ url('/school/division/create') }}">Create a new division</a>&nbsp;
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script> --}}
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
@@ -29,5 +36,6 @@
     <div class="container">
         @yield("content")
     </div>
+
   </body>
 </html>
