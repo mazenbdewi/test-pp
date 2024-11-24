9886998dce05dc922916f0bbafb12580731cf18d
gadeermahmoodhamdan <gadeermahmoodhamdan@gmail.com>
2024-11-23 22:30:55 -0800

    altering Student model for relations

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,17 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    protected  $fillable=[ 
-      
+    protected  $fillable=[
+
    'first_name',
    'middle_name',
    'last_name',
    'mother_name',
    'email' ,
-   'course_id', 
-   'address',          
-   'national_id',        
-   'college_name',       
-   'specialization',     
-   'overall_grade' ];
+   'course_id',
+   'address',
+   'national_id',
+   'college_name',
+   'specialization',
+   'overall_grade',
+   'school_id',
+   'division_id',
+   'level'];
+   public function division()
+    {
+        return $this->belongsToMany('App\Models\Division','division_id');
+    }
+    public function school()
+    {
+        return $this->belongsToMany('App\Models\School','school_id');
+    }
 }
